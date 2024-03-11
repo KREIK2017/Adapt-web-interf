b0868909ae2af598c8aed154d3ef7cd2b99de7c1
Maksim <93036602+KREIK2017@users.noreply.github.com>
2024-03-11 17:53:48 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,39 +1,35 @@
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+var builder = WebApplication.CreateBuilder(args);
 
-class Program
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-	static HttpClient httpClient = new HttpClient();
-	static async Task Main()
-	{
-		APIClient apiClient = new APIClient();
-		ResponseModel<string> response = await apiClient.GetRandomText();
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "WebAPI", Version = "v1" });
+});
+var app = builder.Build();
 
-		// Перевірка, чи відповідь успішна
-		if (response.HttpStatusCode == 200)
-		{
-			Console.WriteLine($"Message: {response.Message}");
-			Console.WriteLine($"HTTP Status Code: {response.HttpStatusCode}");
-
-			// Виведення отриманих даних з API
-			if (response.Data != null)
-			{
-				foreach (var data in response.Data)
-				{
-					Console.WriteLine($"Data from API: {data}");
-				}
-			}
-			else
-			{
-				Console.WriteLine("No data received from API.");
-			}
-		}
-		else
-		{
-			Console.WriteLine($"Error: {response.Message}");
-			Console.WriteLine($"HTTP Status Code: {response.HttpStatusCode}");
-		}
-
-	}
+if (app.Environment.IsDevelopment())
+{
+    // 3 .../swagger/index.html
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI");
+    });
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
