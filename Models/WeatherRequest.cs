3f0c9298f5ee520b2ee7fdba006712028442dc0f
Maksim <93036602+KREIK2017@users.noreply.github.com>
2024-03-17 18:21:51 +0200

    Delete Models/WeatherRequest.cs

--- Models/WeatherRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class WeatherRequest
-    {
-        public int numberOfDays { get; set; }
-        public int minValue { get; set; }
-        public int maxValue { get; set; }
-    }
-}
