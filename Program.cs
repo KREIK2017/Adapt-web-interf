9e6e116208a55845257573b69fd9ddebb943d8bc
Maksim <93036602+KREIK2017@users.noreply.github.com>
2024-03-21 23:04:14 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,39 +1,101 @@
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RESTwebAPI.Models.Auth;
+using RESTwebAPI.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
-class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IProductService, ProductService>();
+builder.Services.AddSingleton<IOrderService, OrderService>();
+builder.Services.AddSingleton<ICategoryService, CategoryService>();
+builder.Services.AddSingleton<IAuthService, AuthService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-	static HttpClient httpClient = new HttpClient();
-	static async Task Main()
-	{
-		APIClient apiClient = new APIClient();
-		ResponseModel<string> response = await apiClient.GetRandomText();
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "WebAPI", Version = "v1" });
+    //7
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
 
-		// Перевірка, чи відповідь успішна
-		if (response.HttpStatusCode == 200)
-		{
-			Console.WriteLine($"Message: {response.Message}");
-			Console.WriteLine($"HTTP Status Code: {response.HttpStatusCode}");
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
+});
+//5
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidateAudience = true,
+            ValidAudience = AuthOptions.AUDIENCE,
+            ValidateLifetime = true,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true,
+         };
+});
+var app = builder.Build();
+// аутентифікація
+app.UseAuthentication();
 
-			// Виведення отриманих даних з API
-			if (response.Data != null)
-			{
-				foreach (var data in response.Data)
-				{
-					Console.WriteLine($"Data from API: {data}");
-				}
-			}
-			else
-			{
-				Console.WriteLine("No data received from API.");
-			}
-		}
-		else
-		{
-			Console.WriteLine($"Error: {response.Message}");
-			Console.WriteLine($"HTTP Status Code: {response.HttpStatusCode}");
-		}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
 
-	}
+        c.OAuthClientId("swagger");
+        c.OAuthAppName("Your API - Swagger");
+    });
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.Map("/login/{username}", (string username) =>
+{
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
+    var jwt = new JwtSecurityToken(
+            issuer: AuthOptions.ISSUER,
+            audience: AuthOptions.AUDIENCE,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)), // время действия 2 минуты
+            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+    return new JwtSecurityTokenHandler().WriteToken(jwt);
+});
+
+
+app.Run();
---
+++ Services/Order/OrderService.cs
@@ -0,0 +1,110 @@
+﻿using RESTwebAPI.Models;
+
+namespace RESTwebAPI.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly List<Order> _orders;
+
+        public OrderService()
+        {
+            _orders = new List<Order>
+            {
+                new Order { OrderId = 1, OrderName = "Смартфон Samsung Galaxy S21", TotalAmount = 50 },
+                new Order { OrderId = 2, OrderName = "Ноутбук HP Pavilion 15", TotalAmount = 100 },
+                new Order { OrderId = 3, OrderName = "Телевізор LG OLED55CX", TotalAmount = 150 },
+                new Order { OrderId = 4, OrderName = "Пилосос Xiaomi Mi Robot Vacuum", TotalAmount = 200 },
+                new Order { OrderId = 5, OrderName = "Ігрова консоль Sony PlayStation 5", TotalAmount = 250 },
+                new Order { OrderId = 6, OrderName = "Холодильник Bosch KGN39VL35", TotalAmount = 300 },
+                new Order { OrderId = 7, OrderName = "Спортивний годинник Garmin Forerunner 945", TotalAmount = 400 },
+                new Order { OrderId = 8, OrderName = "Книга \"Майстер та Маргарита\" Михаїла Булгакова", TotalAmount = 500 },
+                new Order { OrderId = 9, OrderName = "Мультиварка REDMOND SkyCooker", TotalAmount = 600 },
+                new Order { OrderId = 10, OrderName = "Блендер Philips Daily Collection", TotalAmount = 1000 },
+            };
+        }
+        public async Task<ResponseModel<Order>> AddOrderAsync(Order order)
+        {
+            order.OrderId = _orders.Any() ? _orders.Max(p => p.OrderId) + 1 : 1;
+            _orders.Add(order);
+            return new ResponseModel<Order>
+            {
+                Data = order,
+                Success = true,
+                Message = "Order added successfully."
+            };
+        }
+
+        public async Task<ResponseModel<Order>> DeleteOrderAsync(int id)
+        {
+            var orderToRemove = _orders.FirstOrDefault(p => p.OrderId == id);
+            if (orderToRemove == null)
+            {
+                return new ResponseModel<Order>
+                {
+                    Data = null,
+                    Success = false,
+                    Message = $"Order with id {id} not found."
+                };
+            }
+            _orders.Remove(orderToRemove);
+            return new ResponseModel<Order>
+            {
+                Data = orderToRemove,
+                Success = true,
+                Message = $"Order with id {id} deleted successfully."
+            };
+        }
+
+        public async Task<ResponseModel<IEnumerable<Order>>> GetAllOrdersAsync()
+        {
+            return new ResponseModel<IEnumerable<Order>>
+            {
+                Data = _orders,
+                Success = true,
+                Message = "Successfully retrieved all orders."
+            };
+        }
+
+        public async Task<ResponseModel<Order>> GetOrderAsync(int id)
+        {
+            var order = _orders.FirstOrDefault(p => p.OrderId == id);
+            if (order == null)
+            {
+                return new ResponseModel<Order>
+                {
+                    Data = null,
+                    Success = false,
+                    Message = $"Order with id {id} not found."
+                };
+            }
+            return new ResponseModel<Order>
+            {
+                Data = order,
+                Success = true,
+                Message = $"Successfully retrieved order with id {id}."
+            };
+        }
+
+        public async Task<ResponseModel<Order>> UpdateOrderAsync(int id, Order order)
+        {
+            var existingOrder = _orders.FirstOrDefault(p => p.OrderId == id);
+            if (existingOrder == null)
+            {
+                return new ResponseModel<Order>
+                {
+                    Data = null,
+                    Success = false,
+                    Message = $"Order with id {id} not found."
+                };
+            }
+            existingOrder.OrderName = order.OrderName;
+            existingOrder.TotalAmount= order.TotalAmount;
+            return new ResponseModel<Order>
+            {
+                Data = existingOrder,
+                Success = true,
+                Message = $"Order with id {id} updated successfully."
+            };
+        }
+    }
+}
