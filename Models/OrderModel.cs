c89f5f7af3344c7a045a2386dc36ff8cd570e68b
Maksim <93036602+KREIK2017@users.noreply.github.com>
2024-04-15 19:44:14 +0300

    Delete Models directory

--- Models/OrderModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RESTwebAPI.Models
-{
-    public class Order
-    {
-        public int OrderId { get; set; }
-        public string OrderName { get; set; }
-        public decimal TotalAmount { get; set; }
-    }
-}
